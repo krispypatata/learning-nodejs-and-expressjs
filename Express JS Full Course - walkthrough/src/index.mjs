05dacd99f6dcbfde6319787bf07eb4583f70efc1
krispypatata <keithginoel@gmail.com>
2024-04-08 10:44:24 +0800

    route parameters

--- Express JS Full Course - walkthrough/src/index.mjs
+++ Express JS Full Course - walkthrough/src/index.mjs
@@ -8,13 +8,33 @@ app.get("/", (request, response)=> {
     response.status(201).send({ msg: "Hello World!" });
 });
 
+const mockUsers = [
+    { id:1, username: "keith", displayName: "Keith" },
+    { id:2, username: "ginoel", displayName: "Ginoel" },
+    { id:3, username: "gabinete", displayName: "Gabinete" }, 
+];
+
+
 // 2nd route
 app.get('/api/users', (request, response) => {
-    response.send([
-        { id:1, username: "keith", displayName: "Keith" },
-        { id:2, username: "ginoel", displayName: "Ginoel" },
-        { id:3, username: "gabinete", displayName: "Gabinete" }, 
-    ])
+    response.send(mockUsers);
+});
+
+// route parameter
+app.get('/api/users/:id', (request, response) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+    console.log(parsedId);
+    if (isNaN(parsedId)) {
+        return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+    }
+
+    const findUser = mockUsers.find((user) => user.id === parsedId);
+    if (!findUser) {
+        return response.sendStatus(404);
+    }
+
+    return response.send(findUser);
 });
 
 // 3rd route
